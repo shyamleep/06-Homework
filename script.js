ffa20763783bc0d4ee8395119696c65e216c2b7a
Venn Diagram <68201342+shyamleep@users.noreply.github.com>
2021-01-20 22:17:13 -0500

    vars to pull data from API

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ var searchButton = $("#searchButton");
 var searchHistory = $("#searchHistory");
 var currentWeather = $("#current-weather");
 var forecast = $("#forecast");
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=";
+let lat;
+let lon; 
+
 
 function getCurrentWeather() {
     $("#searchButton").on("click", function () {
@@ -16,8 +19,20 @@ function getCurrentWeather() {
             $.ajax({
                 url: queryURL + city + "&units=imperial&appid=e4c96f7efefd249832654c007f8c0ef1",
                 method: "GET",   
+            }).then(function(data) {
+                console.log(data);
+                // get lat and lon 
+                lat = data.city.coord.lat;
+                lon = data.city.coord.lon;
+                // get city info from data
+                var cityName = $("<h3").text(data.city.name);
+                var weatherIcon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + data.list[0].weather[0].weatherIcon + "@2x.png"
+                var cityTemp = data.list[0].main.temp;
+                var cityHumidity = data.list[0].main.humidity;
+                var windSpeed = data.list[0].wind.speed;
+
             })
-            console.log(url)
+            // console.log(url)
         }
     })
 };
